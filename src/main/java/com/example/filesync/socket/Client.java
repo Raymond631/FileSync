1c96b46de3b47ac8e64a0eba7688d1f81fc9e0d9
Raymond <raymond631@qq.com>
2023-05-28 06:03:35 +0800

    修复socket被关闭的问题

--- src/main/java/com/example/filesync/socket/Client.java
+++ src/main/java/com/example/filesync/socket/Client.java
@@ -1,15 +1,19 @@
 package com.example.filesync.socket;
 
 import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
 import com.example.filesync.entity.FolderInfo;
 import com.example.filesync.entity.Message;
+import com.example.filesync.entity.RemoteFolder;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * 发送端
@@ -36,13 +40,76 @@ public class Client {
      */
     public static void sendFileInfo(FolderInfo folderInfo, String destIp) {
         try (Socket socket = new Socket(destIp, 6666); DataOutputStream dos = new DataOutputStream(socket.getOutputStream())) {
-            dos.writeInt(infoTcp);
-            dos.flush();
-
             dos.writeUTF(JSON.toJSONString(folderInfo));
             dos.flush();
+
+            receiveFile(new DataInputStream(socket.getInputStream()));
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 接收文件
+     */
+    public static void receiveFile(DataInputStream dis) throws IOException {
+        RemoteFolder remoteFolder = JSONObject.parseObject(dis.readUTF(), RemoteFolder.class);
+        String basePath = remoteFolder.getLocalPath(); // 读取本地保存路径
+        System.out.println("保存路径：" + basePath);
+        // 读取文件数
+        int numOfFiles = dis.readInt();
+        for (int i = 0; i < numOfFiles; i++) {
+            //读取文件信息
+            JSONObject info = JSON.parseObject(dis.readUTF());
+            String fileName = info.getString("name");
+            long fileLength = info.getLong("length");
+
+            //创建文件实例和文件输出流
+            String path = basePath + fileName;
+            createFileRecursion(path, 0); //递归创建文件
+            File file = new File(path);
+
+            //开始接受文件主体
+            byte[] bytes = new byte[1024];
+            int length = 0;
+            try (FileOutputStream fos = new FileOutputStream(file)) {
+                while (((length = dis.read(bytes, 0, bytes.length)) != -1)) {
+                    fos.write(bytes, 0, length);
+                    fos.flush();
+                    fileLength -= length;
+
+                    if (fileLength == 0) {
+                        break;
+                    }
+                    if (fileLength < bytes.length) {
+                        bytes = new byte[(int) fileLength];
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * 递归创建文件
+     */
+    public static void createFileRecursion(String fileName, Integer height) throws IOException {
+        System.out.println(fileName);
+        Path path = Paths.get(fileName);
+        if (Files.exists(path)) {
+            // 如果文件存在
+            return;
+        }
+        if (Files.exists(path.getParent())) {
+            // 如果父级文件存在，直接创建文件
+            if (height == 0) {
+                Files.createFile(path);
+            } else {
+                Files.createDirectory(path);
+            }
+        } else {
+            createFileRecursion(path.getParent().toString(), height + 1);
+            // 这一步能保证path的父级一定存在了，现在需要把自己也建一下
+            createFileRecursion(fileName, height);
         }
     }
 }
--- src/main/java/com/example/filesync/socket/TcpThread.java
+++ src/main/java/com/example/filesync/socket/TcpThread.java
@@ -10,9 +10,6 @@ import com.example.filesync.service.RemoteFolderService;
 
 import java.io.*;
 import java.net.Socket;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
@@ -35,15 +32,7 @@ public class TcpThread implements Runnable {
     @Override
     public void run() {
         try (InputStream inputStream = client.getInputStream(); DataInputStream dis = new DataInputStream(inputStream)) {
-            int type = dis.readInt();
-            switch (type) {
-                case Client.infoTcp -> {
-                    responseSync(dis);
-                }
-                case Client.fileTcp -> {
-                    receiveFile(dis);
-                }
-            }
+            responseSync(dis);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -72,11 +61,8 @@ public class TcpThread implements Runnable {
         }
         System.out.println(fileInfo);
 
-        try (Socket socket = new Socket(client.getInetAddress().getHostAddress(), 6666); OutputStream outputStream = socket.getOutputStream(); DataOutputStream dos = new DataOutputStream(outputStream)) {
+        try (OutputStream outputStream = client.getOutputStream(); DataOutputStream dos = new DataOutputStream(outputStream)) {
             dos.writeUTF(JSON.toJSONString(remoteFolder));  // 传回foldId和localPath，方便接收端保存
-            dos.flush();
-
-            dos.writeInt(Client.fileTcp);
             dos.flush();
 
             dos.writeInt(fileInfo.size());
@@ -104,69 +90,6 @@ public class TcpThread implements Runnable {
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
-        }
-    }
-
-    /**
-     * 接收文件
-     */
-    public void receiveFile(DataInputStream dis) throws IOException {
-        RemoteFolder remoteFolder = JSONObject.parseObject(dis.readUTF(), RemoteFolder.class);
-        String basePath = remoteFolder.getLocalPath(); // 读取本地保存路径
-        // 读取文件数
-        int numOfFiles = dis.readInt();
-        for (int i = 0; i < numOfFiles; i++) {
-            //读取文件信息
-            JSONObject info = JSON.parseObject(dis.readUTF());
-            String fileName = info.getString("name");
-            long fileLength = info.getLong("length");
-
-            //创建文件实例和文件输出流
-            String path = basePath + fileName;
-            createFileRecursion(path, 0); //递归创建文件
-            File file = new File(path);
-
-            //开始接受文件主体
-            byte[] bytes = new byte[1024];
-            int length = 0;
-            try (FileOutputStream fos = new FileOutputStream(file)) {
-                while (((length = dis.read(bytes, 0, bytes.length)) != -1)) {
-                    fos.write(bytes, 0, length);
-                    fos.flush();
-                    fileLength -= length;
-
-                    if (fileLength == 0) {
-                        break;
-                    }
-                    if (fileLength < bytes.length) {
-                        bytes = new byte[(int) fileLength];
-                    }
-                }
-            }
-        }
-    }
-
-    /**
-     * 递归创建文件
-     */
-    public void createFileRecursion(String fileName, Integer height) throws IOException {
-        System.out.println(fileName);
-        Path path = Paths.get(fileName);
-        if (Files.exists(path)) {
-            // 如果文件存在
-            return;
-        }
-        if (Files.exists(path.getParent())) {
-            // 如果父级文件存在，直接创建文件
-            if (height == 0) {
-                Files.createFile(path);
-            } else {
-                Files.createDirectory(path);
-            }
-        } else {
-            createFileRecursion(path.getParent().toString(), height + 1);
-            // 这一步能保证path的父级一定存在了，现在需要把自己也建一下
-            createFileRecursion(fileName, height);
         }
     }
 }
